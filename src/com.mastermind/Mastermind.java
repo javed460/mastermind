99c35da8a4ea6b49ad7a6d16c38dc791921ddf72
Javed Bagwan <javed460@gmail.com>
2023-12-31 08:30:03 +0530

    Calling API from main class

--- src/com.mastermind/Mastermind.java
+++ src/com.mastermind/Mastermind.java
@@ -1,4 +1,11 @@
 package com.mastermind;
 
+import static com.mastermind.RandomGeneratorAPI.getRandomNumberCombination;
+
 public class Mastermind {
+
+    public static void main(String[] args) {
+        String[] randomNumberCombination = getRandomNumberCombination();
+
+    }
 }
