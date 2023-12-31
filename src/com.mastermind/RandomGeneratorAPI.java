2b42b8b4eb502d32f09501ce8bb3efdbc1c12cc0
Javed Bagwan <javed460@gmail.com>
2023-12-31 08:29:23 +0530

    Implemented API call to get random number combination

--- src/com.mastermind/RandomGeneratorAPI.java
+++ src/com.mastermind/RandomGeneratorAPI.java
@@ -1,4 +1,28 @@
 package com.mastermind;
 
-public class RandomGeneratorAPI {
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public final class RandomGeneratorAPI {
+
+    public static String[] getRandomNumberCombination() {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                String randomNumbers = response.body();
+                return randomNumbers.split("\n");
+            } else {
+                throw new RuntimeException("Failed to get random numbers - status code" + response.statusCode());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to get random numbers" + e.getMessage());
+        }
+    }
 }
